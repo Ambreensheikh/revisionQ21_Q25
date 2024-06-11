2cd65d22215dfd2b76741e9ef5cf4111f2e82ab7
Ambreen Sheikh <ambreensheikh900@gmail.com>
2024-06-11 23:29:16 +0500

    first commit

--- index.js
+++ index.js
@@ -1,83 +1,36 @@
-//Q26 as it is Q25
-//Q27 :alian colors
-//VERSION 1
-let alian_color = "green";
-if (alian_color === "green") {
-    console.log("The player earned 5 points");
+//Q_36//make a callback function
+//function make_shirt(size:string, message:string){
+// console.log(`Shitr size is ${size} ,and a message ${message} print on it`);    
+//}
+//make_shirt("Large" , "'I love Pakistan !'");
+//Q_37
+function make_shirt(size = "large", message = "I love TypeScript") {
+    console.log(`Shirt size is ${size} ,and a message ${message} print on it`);
 }
-else if (alian_color === "Yellow") {
-    console.log("The player earned 10 points");
+make_shirt(); //default shirt size,message
+make_shirt("Medium"); //default message
+make_shirt("Small", "Dive Into Coding");
+//Q_38
+function describe_city(city, country = "Pakistan") {
+    console.log(`city ${city} belongs to country ${country}`);
 }
-else if (alian_color === "Red") {
-    console.log("The player earned 15 points");
+describe_city("Karachi");
+describe_city("Lahore");
+describe_city("Tokyo", "Japan");
+//Q_39
+function city_country(city, country) {
+    console.log(`${city} , ${country}`);
 }
-//if alian  color is Yellow print a message of 10 points
-//VERSION 2
-alian_color = "Yellow";
-if (alian_color === "green") {
-    console.log("The player earned 5 points");
+city_country("Gazza", "Palestine");
+city_country("Karachi", "Pakistan");
+city_country("Paris", "France");
+function make_album(artistName, albumTitle, songs) {
+    return ` ${artistName}, ${albumTitle} ,${songs}`;
 }
-else if (alian_color === "Yellow") {
-    console.log("The player earned 10 points");
-}
-else if (alian_color === "Red") {
-    console.log("The player earned 15 points");
-}
-//if alian  color is red print a message of 15 points
-alian_color = "Red";
-if (alian_color === "green") {
-    console.log("The player earned 5 points");
-}
-else if (alian_color === "Yellow") {
-    console.log("The player earned 10 points");
-}
-else if (alian_color === "Red") {
-    console.log("The player earned 15 points");
-}
-//Q28 : stages of life
-let age = 18; //set the value of variable age
-if (age < 2) {
-    console.log("Yhe person is a baby !"); //2 years old but less than 4
-}
-else if (age >= 2 && age < 4) {
-    console.log("The person is a toddler !"); //
-}
-else if (age >= 4 && age < 13) {
-    console.log("The person is a kid !");
-}
-else if (age >= 13 && age < 20) {
-    console.log("The person is a teenager !");
-}
-else if (age >= 20 && age < 65) {
-    console.log("The person is an adult !");
-}
-else {
-    console.log("The person is an elder !");
-}
-;
-//Q29 : 
-let favFruits = ["Grapes", "Banana", "Mango"];
-if (favFruits.includes("Grapes")) {
-    console.log("I really like Grapes");
-}
-if (favFruits.includes("Mango")) {
-    console.log("I really like Mango");
-}
-if (favFruits.includes("Kiwi")) {
-    console.log("I really like Kiwi");
-}
-else {
-    console.log("I dont like Kiwi");
-}
-//Q 30 :make array of 5 user name
-let userName = ["Admin", "Ali", "Usman", "Mahad", "Hamza"];
-//write greeting statement
-for (let i = 0; i < userName.length; i++) {
-    if (userName[i] === "Admin") {
-        console.log(`Hello ${userName[i]} , would you like to check reports !`);
-    }
-    else {
-        console.log(`Hello , ${userName[i]}thank you for logging in again !`);
-    }
-}
+let artistName1 = make_album("Atif Aslam", "coke Studio", 2019);
+let artistName2 = make_album("Rahat Fateh ali", "seher", 2020);
+let artistName3 = make_album("Ali Zafar", "Jhoom", 2017);
+console.log(artistName1);
+console.log(artistName2);
+console.log(artistName3);
 export {};
--- index.ts
+++ index.ts
@@ -1,74 +1,59 @@
-//Q26 as it is Q25
-//Q27 :alian colors
-//VERSION 1
-let alian_color:string = "green";
-if(alian_color === "green"){
-    console.log("The player earned 5 points");
- } else if(alian_color === "Yellow"){
-    console.log("The player earned 10 points");
-    }else if(alian_color === "Red"){
-        console.log("The player earned 15 points");
-        }
-//if alian  color is Yellow print a message of 10 points
-//VERSION 2
-alian_color = "Yellow";
-if(alian_color === "green"){
-    console.log("The player earned 5 points");
- } else if(alian_color === "Yellow"){
-    console.log("The player earned 10 points");
-    }else if(alian_color === "Red"){
-        console.log("The player earned 15 points");
-        }
-//if alian  color is red print a message of 15 points
-alian_color = "Red";
-if(alian_color === "green"){
-    console.log("The player earned 5 points");
- } else if(alian_color === "Yellow"){
-    console.log("The player earned 10 points");
-    }else if(alian_color === "Red"){
-        console.log("The player earned 15 points");
-        }
 
-        //Q28 : stages of life
-let age :number = 18;  //set the value of variable age
-if(age < 2){
-    console.log("Yhe person is a baby !");//2 years old but less than 4
-}else if(age >= 2 && age < 4){
-    console.log("The person is a toddler !");//
-}else if(age >= 4 && age < 13){
-    console.log("The person is a kid !");
-}else if(age >= 13 && age < 20){
-    console.log("The person is a teenager !");
-}else if(age >= 20 && age <65){
-    console.log("The person is an adult !");
-}else{
-    console.log("The person is an elder !");
-};
+//Q_36//make a callback function
+//function make_shirt(size:string, message:string){
+   // console.log(`Shitr size is ${size} ,and a message ${message} print on it`);    
+  //}
+//make_shirt("Large" , "'I love Pakistan !'");
 
-//Q29 : 
- let favFruits:string[]= ["Grapes", "Banana", "Mango"];
- if(favFruits.includes("Grapes")){
-    console.log("I really like Grapes");
- }if(favFruits.includes("Mango")){
-    console.log("I really like Mango");
- }if(favFruits.includes("Kiwi")){
-    console.log("I really like Kiwi");
-}else{
-    console.log("I dont like Kiwi");
-    
+//Q_37
+function make_shirt(size:string ="large", message:string="I love TypeScript"){
+    console.log(`Shirt size is ${size} ,and a message ${message} print on it`);    
 }
-//Q 30 :make array of 5 user name
-let userName:string[] = ["Admin", "Ali", "Usman","Mahad","Hamza"];
-//write greeting statement
-for(let i=0; i< userName.length; i++){
-    if(userName[i] === "Admin"){
-    console.log(`Hello ${userName[i]} , would you like to check reports !`);
-    }else{
-        console.log(`Hello , ${userName[i] }thank you for logging in again !`);
-        
+make_shirt();//default shirt size,message
+make_shirt("Medium");//default message
+make_shirt("Small" , "Dive Into Coding");
+
+//Q_38
+function describe_city(city:string, country:string="Pakistan"){
+    console.log(`city ${city} belongs to country ${country}`);
     }
-    
+    describe_city("Karachi");
+    describe_city("Lahore");
+    describe_city("Tokyo" , "Japan");
+//Q_39
+function city_country(city:string, country:string){
+    console.log(`${city} , ${country}`);
 }
+city_country("Gazza" , "Palestine");
+city_country( "Karachi", "Pakistan");
+city_country("Paris", "France");
+//Q_40
+interface album{
+    artistName:string;
+    albumTitle:string;
+    songs :number
+}
+function make_album(artistName:string,albumTitle:string, songs :number){
+    return` ${artistName}, ${albumTitle} ,${songs}`
+}
+let artistName1 = make_album("Atif Aslam", "coke Studio", 2019);
+let artistName2 = make_album("Rahat Fateh ali", "seher", 2020);   
+let artistName3 = make_album("Ali Zafar", "Jhoom", 2017);
+
+console.log(artistName1);
+console.log(artistName2);
+console.log(artistName3);
+
+
+
+
+    
+  
+    
+       
+    
+    
+
     
 
 
